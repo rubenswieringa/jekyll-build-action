cfe80e60ceb693030b0426d4062c354ea22bfab6
Ruben Swieringa <ruben.swieringa@gmail.com>
2022-08-18 20:35:42 +0100

    WIP: --drafts in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ chmod -R a+w /github/workspace
 # Build the Jekyll site
 ####################################################
 
-jekyll build --trace
+jekyll build --trace --drafts
 
 ####################################################
 # Build completed
